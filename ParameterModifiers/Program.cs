1294e3db5d64f6108e33e0668d23e5a388e91e79
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2024-09-25 20:36:26 -0500

    Began writing some unit tests for ParameterModifiers. Far from finished but there's a good start. Also, fixed some formatting and structure for some files in the project.

--- ParameterModifiers/Program.cs
+++ ParameterModifiers/Program.cs
@@ -1,22 +1,23 @@
-﻿namespace ParameterModifiers;
-
-class Program
+﻿namespace ParameterModifiers
 {
-    static void Main(string[] args)
+    public class Program
     {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void SplitNameIntoParts(string input, out string title, out string first, out string middle, out string last, out string suffix)
+        {
+
+        }
+
+        public static void ReverseInPlace(string s)
+        {
+
+        }
 
     }
-
-    public static void SplitNameIntoParts(string input, string title, string first, string middle, string last, string suffix)
-    {
-
-    }
-
-    public static void ReverseInPlace(string s) 
-    {
-
-    }
-
 }
 
 
---
+++ UnitTests/ParameterModifiersTests.cs
@@ -0,0 +1,44 @@
+﻿using ParameterModifiers;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ParameterModifiersTests
+    {
+        // UNIT TEST IN PROGRESS - DO NOT UNCOMMENT!!
+
+        //[TestMethod]
+        //public void TestSplitNameIntoPartsTitle()
+        //{
+        //    string input = "Dr. David Powell";
+        //    string first, middle, last, title, suffix;
+        //    Program.SplitNameIntoParts(input, out title, out first, out middle, out last, out suffix);
+        //    Assert.AreEqual("Dr.", title);
+
+        //    input = "Mr. David Powell";
+        //    first = null; middle = null; last = null; title = null; suffix = null;
+        //    Program.SplitNameIntoParts(input, out title, out first, out middle, out last, out suffix);
+        //    Assert.AreEqual("Mr.", title);
+
+        //    input = "Mrs. Cathy Powell";
+        //    first = null; middle = null; last = null; title = null; suffix = null;
+        //    Program.SplitNameIntoParts(input, out title, out first, out middle, out last, out suffix);
+        //    Assert.AreEqual("Mrs.", title);
+
+        //    input = "Ms. Cathy Powell";
+        //    first = null; middle = null; last = null; title = null; suffix = null;
+        //    Program.SplitNameIntoParts(input, out title, out first, out middle, out last, out suffix);
+        //    Assert.AreEqual("Mr.", title);
+
+        //    input = "Mr. David Powell";
+        //    first = null; middle = null; last = null; title = null; suffix = null;
+        //    Program.SplitNameIntoParts(input, out title, out first, out middle, out last, out suffix);
+        //    Assert.AreEqual("Mr.", title);
+
+        //    input = "Mr. David Powell";
+        //    first = null; middle = null; last = null; title = null; suffix = null;
+        //    Program.SplitNameIntoParts(input, out title, out first, out middle, out last, out suffix);
+        //    Assert.AreEqual("Mr.", title);
+    //}
+}
+}
